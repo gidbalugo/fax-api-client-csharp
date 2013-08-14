98dd7b45074872a08771b5eca92c74456cf98793
Kreisa Moralde <breisa.moralde@orangeandbronze.com>
2013-08-15 04:57:10 +0800

    added samples for other methods.

--- csharpclient/csharpclient/Program.cs
+++ csharpclient/csharpclient/Program.cs
@@ -17,30 +17,96 @@ namespace csharpclient
 
             //create an array of api fax documents.
             apiFaxDocument[] apiFaxDocuments;
-            apiFaxDocuments = new apiFaxDocument[1] {apiFaxDocument};
+            apiFaxDocuments = new apiFaxDocument[1] { apiFaxDocument };
 
             //create a new fax message.
-            apiFaxMessage apiFaxMessage = new apiFaxMessage();
-            apiFaxMessage.MessageRef = "test-1-1-1";
-            apiFaxMessage.SendTo = "6011111111";
-            apiFaxMessage.SendFrom = "Test Fax";
-            apiFaxMessage.Resolution = faxResolution.normal;
-            apiFaxMessage.Documents = apiFaxDocuments;
+            apiFaxMessage apiFaxMessage1 = new apiFaxMessage();
+            apiFaxMessage1.MessageRef = "test-1-1-1";
+            apiFaxMessage1.SendTo = "6011111111";
+            apiFaxMessage1.SendFrom = "Test fax";
+            apiFaxMessage1.Resolution = faxResolution.normal;
+            apiFaxMessage1.Documents = apiFaxDocuments;
+
+            //create another fax message.
+            apiFaxMessage apiFaxMessage2 = new apiFaxMessage();
+            apiFaxMessage2.MessageRef = "test-1-1-1";
+            apiFaxMessage2.SendTo = "6011111111";
+            apiFaxMessage2.SendFrom = "Test fax";
+            apiFaxMessage2.Resolution = faxResolution.normal;
+            apiFaxMessage2.Documents = apiFaxDocuments;
 
             //create an array of api fax messages.
             apiFaxMessage[] apiFaxMessages;
-            apiFaxMessages = new apiFaxMessage[1] {apiFaxMessage};
-
-            //create an instance of sendFax request.
-            sendFaxRequest sendFaxRequest = new sendFaxRequest();
-            sendFaxRequest.FaxMessages = apiFaxMessages;
+            apiFaxMessages = new apiFaxMessage[2] { apiFaxMessage1, apiFaxMessage2 };
 
             //create a new instance of ApiService.
             ApiService apiClient = new ApiService("https://test.api.monopond.com/fax/soap/v2");
-            //ApiService apiClient = new ApiService("https://api.monopond.com/fax/soap/v2");
-            
-            //call the sendFax method.
-            sendFaxResponse response = apiClient.SendFax(sendFaxRequest, "username", "password");
+            //ApiService apiClient = new ApiService("http://192.168.1.201:8000/fax/soap/v2");
+
+            //TODO: change user credentials
+            string username = "username";
+            string password = "password";
+
+            resumeFaxSample(apiClient, username, password);
+            pauseFaxSample(apiClient, username, password);
+            stopFaxSample(apiClient, username, password);
+            sendFaxSample(apiFaxMessages, apiClient, username, password);
+            faxStatusSample(apiClient, username, password);
         }
+
+        private static void resumeFaxSample(ApiService apiClient, string username, string password)
+        {
+            //create a new instace of resumeFax request.
+            resumeFaxRequest resumeFaxRequest = new resumeFaxRequest();
+            resumeFaxRequest.BroadcastRef = "broadcast-ref-1";
+
+            //call the resumeFax method.
+            resumeFaxResponse resumeFaxResponse = apiClient.ResumeFax(resumeFaxRequest, username, password);
+        }
+
+        private static void pauseFaxSample(ApiService apiClient, string username, string password)
+        {
+            //create a new instance of pauseFax request.
+            pauseFaxRequest pauseFaxRequest = new pauseFaxRequest();
+            pauseFaxRequest.BroadcastRef = "broadcast-ref-1";
+
+            //call the pauseFax method.
+            pauseFaxResponse pauseFaxResponse = apiClient.PauseFax(pauseFaxRequest, username, password);
+        }
+
+        private static void stopFaxSample(ApiService apiClient, string username, string password)
+        {
+            //create a new instance of stopFax request.
+            stopFaxRequest stopFaxRequest = new stopFaxRequest();
+            stopFaxRequest.BroadcastRef = "broadcast-ref-1";
+
+            //call the stopFax method.
+            stopFaxResponse stopFaxResponse = apiClient.StopFax(stopFaxRequest, username, password);
+        }
+
+        private static void sendFaxSample(apiFaxMessage[] apiFaxMessages, ApiService apiClient, string username, string password)
+        {
+            //create a new instance of sendFax request.
+            sendFaxRequest sendFaxRequest = new sendFaxRequest();
+            sendFaxRequest.FaxMessages = apiFaxMessages;
+
+            //call the sendFax method.
+            sendFaxResponse sendFaxResponse = apiClient.SendFax(sendFaxRequest, username, password);
+        }
+
+        private static void faxStatusSample(ApiService apiClient, string username, string password)
+        {
+            //create a new instance of fax status request.
+            faxStatusRequest faxStatusRequest = new faxStatusRequest();
+            faxStatusRequest.BroadcastRef = "test-ref";
+            faxStatusRequest.Verbosity = faxStatusLevel.brief;
+
+            //call the faxStatus method.
+            faxStatusResponse response = apiClient.FaxStatus(faxStatusRequest, username, password);
+        }
+
+
     }
+
+
 }
